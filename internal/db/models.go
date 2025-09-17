454d1b9804fa6a8302055da47a3add70ab999cd4
Kir Ivanov <kirivanov777@gmail.com>
2025-09-17 22:04:56 +0300

    feat: add json tag to models

--- internal/db/models.go
+++ internal/db/models.go
@@ -5,60 +5,60 @@ import "github.com/uptrace/bun"
 type Department struct {
 	bun.BaseModel `bun:"table:departments"`
 
-	ID        uint8  `bun:",pk,autoincrement"`
-	ShortName string `bun:"type:varchar(15)"`
-	FullName  string
-	Faculty   *string `bun:"type:enum('ИВТФ','ИФФ','ЭМФ')"` // todo: replace string with go native enum
+	ID        uint8  `bun:",pk,autoincrement" json:"department"`
+	ShortName string `bun:"type:varchar(15)" json:"shortName"`
+	FullName  string `json:"fullName"`
+	Faculty   *string `bun:"type:enum('ИВТФ','ИФФ','ЭМФ')" json:"faculty"` // todo: replace string with go native enum
 }
 
 type Room struct {
 	bun.BaseModel `bun:"table:rooms"`
 
-	ID       uint16 `bun:",pk,autoincrement"`
-	Building string `bun:"type:enum('А','Б','В','Г','Д','С'),notnull"`
-	Floor    uint8  `bun:",notnull"`
-	Number   uint8  `bun:",notnull"`
-	Label    string `bun:"type:varchar(31)"`
+	ID       uint16 `bun:",pk,autoincrement" json:"id"`
+	Building string `bun:"type:enum('А','Б','В','Г','Д','С'),notnull" json:"building"`
+	Floor    uint8  `bun:",notnull" json:"floor"`
+	Number   uint8  `bun:",notnull" json:"number"`
+	Label    string `bun:"type:varchar(31)" json:"label"`
 }
 
 type Professor struct {
 	bun.BaseModel `bun:"table:professors"`
 
-	ID           uint16     `bun:",pk,autoincrement"`
-	FirstName    string     `bun:"type:varchar(63)"`
-	Patronymic   string     `bun:"type:varchar(63)"`
-	Surname      string     `bun:"type:varchar(63)"`
-	DepartmentID uint8      `bun:",notnull"`
-	Department   Department `bun:"rel:belongs-to,join:department_id=id,notnull"`
+	ID           uint16     `bun:",pk,autoincrement" json:"id"`
+	FirstName    string     `bun:"type:varchar(63)" json:"firstName"`
+	Patronymic   string     `bun:"type:varchar(63)" json:"patronymic"`
+	Surname      string     `bun:"type:varchar(63)" json:"surname"`
+	DepartmentID uint8      `bun:",notnull" json:"departmentId"`
+	Department   Department `bun:"rel:belongs-to,join:department_id=id,notnull" json:"department"`
 }
 
 type Group struct {
 	bun.BaseModel `bun:"table:groups"`
 
-	ID           uint16 `bun:",pk,autoincrement"`
-	Label        string `bun:"type:varchar(15)"`
-	StartYear    uint8  `bun:",notnull"`
-	Number       uint8  `bun:",notnull"`
-	Subgroup     uint8  `bun:",notnull"`
-	DepartmentID uint8  `bun:",notnull"`
+	ID           uint16 `bun:",pk,autoincrement" json:"id"`
+	Label        string `bun:"type:varchar(15)" json:"label"`
+	StartYear    uint8  `bun:",notnull" json:"startYear"`
+	Number       uint8  `bun:",notnull" json:"number"`
+	Subgroup     uint8  `bun:",notnull" json:"subgroup"`
+	DepartmentID uint8  `bun:",notnull" json:"departmentId"`
 
-	Department Department `bun:"rel:belongs-to,join:department_id=id,notnull"`
-	Records    []Record   `bun:"m2m:group_records,join:Group=Record"`
+	Department Department `bun:"rel:belongs-to,join:department_id=id,notnull" json:"department"`
+	Records    []Record   `bun:"m2m:group_records,join:Group=Record" json:"records"`
 }
 
 type Record struct {
 	bun.BaseModel `bun:"table:records"`
 
-	ID          uint32 `bun:"type:mediumint unsigned,pk,autoincrement"`
-	Label       string `bun:",notnull"`
-	Kind        string `bun:"type:enum('lab','sem','lec'),notnull"`
-	Position    uint8  `bun:",notnull"`
-	ProfessorID uint16 `bun:",notnull"`
-	RoomID      uint16 `bun:",notnull"`
+	ID          uint32 `bun:"type:mediumint unsigned,pk,autoincrement" json:"id"`
+	Label       string `bun:",notnull" json:"label"`
+	Kind        string `bun:"type:enum('lab','sem','lec'),notnull" json:"kind"`
+	Position    uint8  `bun:",notnull" json:"position"`
+	ProfessorID uint16 `bun:",notnull" json:"professorId"`
+	RoomID      uint16 `bun:",notnull" json:"roomId"`
 
-	Professor Professor `bun:"rel:belongs-to,join:professor_id=id,notnull"`
-	Room      Room      `bun:"rel:belongs-to,join:room_id=id,notnull"`
-	Groups    []Group   `bun:"m2m:group_records,join:Record=Group"`
+	Professor Professor `bun:"rel:belongs-to,join:professor_id=id,notnull" json:"professor"`
+	Room      Room      `bun:"rel:belongs-to,join:room_id=id,notnull" json:"room"`
+	Groups    []Group   `bun:"m2m:group_records,join:Record=Group" json:"groups"`
 }
 
 type GroupRecord struct {
